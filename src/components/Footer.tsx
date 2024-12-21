0c7568b569acf0b8f1b73d934fa497c56ce9e484
AubreyAmoah <2gee419@gmail.com>
2024-12-21 20:01:57 +0000

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&copy; 2024 AubreyAmoah. All Rights Reserved.</p>
+      <div className="footer__links">
+        {["About", "privacy policy", "Licensing", "Contact"].map((item) => (
+          <Link
+            key={item}
+            href={`/${item.toLocaleLowerCase().replace(" ", "-")}`}
+            className="footer__link"
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
